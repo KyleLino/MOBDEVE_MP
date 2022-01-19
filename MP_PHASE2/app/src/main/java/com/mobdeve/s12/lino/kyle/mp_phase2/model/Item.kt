580171fbde24c870fb0533d2f1d38be47015a1b9
KyleLino <kyle_lino@dlsu.edu.ph>
2022-01-19 18:13:51 +0800

    delete old mp
    to be replaced

--- MP_PHASE2/app/src/main/java/com/mobdeve/s12/lino/kyle/mp_phase2/model/Item.kt
+++
@@ -1,7 +0,0 @@
-package com.mobdeve.s12.lino.kyle.mp_phase2.model
-
-class Item (
-            var textname:String,
-            var textprice:String,
-            var textdescription:String,
-            )
