22e4ce831a5d4b655dc72ec241cca84bba2b4c1a
KyleLino <kyle_lino@dlsu.edu.ph>
2022-02-10 01:34:03 +0800

    WISHLIST

---
+++ MobdeveMP/app/src/main/java/com/mobdeve/s12/anigan/lino/mobdevemp/AddWishListActivity.kt
@@ -0,0 +1,69 @@
+package com.mobdeve.s12.anigan.lino.mobdevemp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.mobdeve.s12.anigan.lino.mobdevemp.databinding.ActivityAddWishListBinding
+
+class AddWishListActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddWishListBinding
+    lateinit var database: DatabaseReference
+
+    var TAG = "ADDWISHLISTACTIVITY"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_wish_list)
+
+        var bundle = intent.extras
+
+        binding = ActivityAddWishListBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.addbutton!!.setOnClickListener{
+
+            val name = binding.itemnameinput.text.toString()
+
+            var owner = bundle!!.getString("username")
+            Log.i(TAG,"$owner")
+
+            if (name != ""){
+
+                database = FirebaseDatabase.getInstance().getReference("UserWishListItem")
+                val UserWishList = owner?.let { it1 -> UserWishList(name, it1) }
+                database.child(name).setValue(UserWishList).addOnSuccessListener {
+
+                    Log.i(TAG,"here")
+
+                    binding.itemnameinput.text.clear()
+
+                    Toast.makeText(this, "user wishlist item created", Toast.LENGTH_SHORT).show()
+
+                    val gotoWishlistActivity = Intent(applicationContext, WishlistActivity::class.java)
+                    var bundle = Bundle()
+                    bundle.putString("username", owner)
+                    gotoWishlistActivity.putExtras(bundle)
+                    startActivity(gotoWishlistActivity)
+
+                }.addOnFailureListener {
+
+                    Log.i(TAG,"here!")
+                    Toast.makeText(this, "user register FAILED", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this, "fill up all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding!!.profileback!!.setOnClickListener {
+            val gotoProfileActivity = Intent(applicationContext, ProfileActivity ::class.java)
+            startActivity(gotoProfileActivity )
+        }
+    }
+}
--- MobdeveMP/app/src/main/java/com/mobdeve/s12/anigan/lino/mobdevemp/WishlistActivity.kt
+++ MobdeveMP/app/src/main/java/com/mobdeve/s12/anigan/lino/mobdevemp/WishlistActivity.kt
@@ -1,12 +1,33 @@
 package com.mobdeve.s12.anigan.lino.mobdevemp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s12.anigan.lino.mobdevemp.databinding.ActivityAddWishListBinding
+import com.mobdeve.s12.anigan.lino.mobdevemp.databinding.ActivityMainBinding
+import com.mobdeve.s12.anigan.lino.mobdevemp.databinding.ActivityWishlistBinding
 
 class WishlistActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityWishlistBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wishlist)
+
+        binding = ActivityWishlistBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        var bundle = intent.extras
+        var bundleusername = bundle!!.getString("username")
+
+        binding.addwishlist.setOnClickListener {
+            val gotoAddWishListActivity = Intent(applicationContext, AddWishListActivity::class.java)
+            var bundle = Bundle()
+            bundle.putString("username", bundleusername)
+            gotoAddWishListActivity.putExtras(bundle)
+            startActivity(gotoAddWishListActivity)
+        }
     }
 
 
